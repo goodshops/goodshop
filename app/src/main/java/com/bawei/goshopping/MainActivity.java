37839688444d12313bed6ecfbd2e38cdaf55b418
zhaoshihao <18611004563@163.com>
2016-12-26 21:10:44 +0800

    second

--- app/src/main/java/com/bawei/goshopping/MainActivity.java
+++ app/src/main/java/com/bawei/goshopping/MainActivity.java
@@ -10,7 +10,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initData();
     }
+
+    private void initData() {
+    }
+
     private void initView() {
 
     }
